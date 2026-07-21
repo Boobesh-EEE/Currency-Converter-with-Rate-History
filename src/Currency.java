53e170b46fe9e3a51a4b9ef2fee2bf8ed1e13644
Boobesh K <boobeshkaruppusamy@gmail.com>
2026-07-21 11:53:14 +0530

    Initial project structure

---
+++ src/Currency.java
@@ -0,0 +1,28 @@
+public class Currency {
+
+    private String code;
+    private String name;
+    private double rate;
+
+    public Currency(String code, String name, double rate) {
+        this.code = code;
+        this.name = name;
+        this.rate = rate;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+}
